93bd69e686c77f65143c18b7df81cfa850a439a9
Aapo Kinnunen <aapo.kinnunen@edu.riveria.fi>
2024-02-19 12:26:06 +0200

    fr

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res)=> {
+    res.send("Seeking truths beyond meaning of life, you will find 43")
+});
+app.listen(PORT, ()=> {
+    console.log(`Palvelinta ajetaan osoitteesta http://localhost:${port}`)
+});
