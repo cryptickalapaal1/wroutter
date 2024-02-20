1ccb4d3ea9ffa829549b07ed21f81035d83fb9cb
Aapo Kinnunen <aapo.kinnunen@edu.riveria.fi>
2024-02-20 08:48:24 +0200

    dsgf

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res)=> {
-    res.send("Seeking truths beyond meaning of life, you will find 43")
+    res.send("Seeking truths beyond meaning of life, you will find 43.")
 });
 app.listen(PORT, ()=> {
     console.log(`Palvelinta ajetaan osoitteesta http://localhost:${PORT}`)
